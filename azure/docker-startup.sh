cd90d313c6e937b24ba29b1f2ad3a2a19c6f1222
Patrick Bills <billspat@msu.edu>
2020-11-20 08:16:37 -0500

    WIP dockerfile for azure, almost working

---
+++ azure/docker-startup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+# custom startup script the Dockerfile entrypoint
+# currently not used. Azure Dockerfiles have over-complete startup scripts to 
+# accommodate many different types of applications
+
+# put versions in the logs
+python --version
+pip --version
+
+# ensure ssh service is going using systemd 
+echo "Starting SSH ..."
+service ssh start
+
+# Get environment variables to show up in SSH session
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+
+cd $HOME_SITE
+# flask run -p 8000
+export GUNICORN_CMD_ARGS="--bind=0.0.0.0  --timeout 36000 --log-file=- --workers=2 --threads=4 --worker-class=gthread"
+gunicorn --timeout 36000 --bind=0.0.0.0:8000 app:app
