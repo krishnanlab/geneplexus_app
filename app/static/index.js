40cc5739ccc432b1aca49d6792d57608892a1d35
krum <krum@msu.edu>
2020-12-11 11:13:53 -0500

    Fixed issue with file selection input

--- app/static/index.js
+++ app/static/index.js
@@ -1,15 +1,32 @@
-$(document).ready(
+$('[data-toggle="popover"]').popover();
+
+$('#button').click(function(){
+   $("input[type='file']").trigger('click');
+})
+
+
+$('input:file').change(
     function(){
-        $('[data-toggle="popover"]').popover();
+        if ($(this).val()) {
+            $('#val').text(this.value.replace(/C:\\fakepath\\/i, ''))
+            $('input:submit').attr('disabled',false);
+            var file = this.files[0];
+            uploadFile(file);
+    }
+});
 
-        $("#network").attr('disabled',true);
-
-        $('input:file').change(
-            function(){
-                if ($(this).val()) {
-                    $('input:submit').attr('disabled',false);
-                    $('#network').attr('disabled',false);
-                    $("#network").val('BioGRID');
-                }
-            });
+function uploadFile(file){
+    var formData = new FormData();
+    formData.append('formData', file);
+    $.ajax({
+        url: '/upload',  //Server script to process data
+        type: 'POST',
+        data: formData,
+        contentType: false,
+        processData: false
+        //Ajax events
+        //success: function(html){
+        //    alert(html);
+        //}
     });
+}
