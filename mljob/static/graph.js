2472818617623ac86133a8afde249a2c4f9ff51c
Newsted, Jacob <newsted1@msu.edu>
2022-02-17 14:00:47 -0500

    Rough but includes sticky nodes and a slightly broken version of a slider

--- mljob/static/graph.js
+++ mljob/static/graph.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
-  graph_aspect_ratio = 2
+  graph_aspect_ratio = 2;
   $('body').on('click', '#graph_tab', function() {
+    
     var width = $('.result_container').width() * (8/12);
     console.log(width);
     var height = width / graph_aspect_ratio;
@@ -11,40 +12,37 @@ $( document ).ready(function() {
   });
 
   var allNodes = dataset.nodes.sort((a, b) => (a.Probability - b.Probability))
-  console.log(dataset);
-  console.log(allNodes);
   //var width = 1080;
   //var height = 600;
   var width = $('.result_container').width() * (8/12);
   console.log(width);
   var height = width / graph_aspect_ratio;
   console.log(height);
-
+  selectedNode = null;
 
   const svg = d3.select('svg');
   /*const svg = d3.select('#graph_area')
     .append('svg')
     .attr('width', width)
     .attr('height', height);*/
-  const g = svg.append('g');
+  //const g = svg.append('g');
 
-  $('#edge_slider').slider({
+  $('#node_slider').slider({
     range: true,
     min: 0.00,
     max: 1.00,
     values: [0.00, 1.00],
     step: 0.01,
     slide: function( event, ui ) {
-      console.log(ui.values);
-      $('#edge_lower').val(ui.values[0].toFixed(2));
-      $('#edge_upper').val(ui.values[1].toFixed(2));
+      $('#node_lower').val(ui.values[0].toFixed(2));
+      $('#node_upper').val(ui.values[1].toFixed(2));
+      modifyNodeCount(ui.values[0], ui.values[1]);
     }
   });
-  $('#node_slider').slider();
 
   const forceProperties = {
     charge: {
-      strength: -1000,
+      strength: -100,
     },
     collide: {
       strength: 0.3,
@@ -67,7 +65,7 @@ $( document ).ready(function() {
 
 
   //const linkElements = svg.append("g")
-  const linkElements = g
+  linkElements = svg.append('g')
           .attr("class", "links")
           .selectAll("line")
           .data(dataset.links)
@@ -76,19 +74,30 @@ $( document ).ready(function() {
           .style("stroke-width", function(d) { return (d.weight); });
 
   //const nodeElements = svg.append('g')
-  const nodeElements = g
+  nodeElements = svg.append('g')
+    .attr('class', 'nodes')
     .selectAll('circle')
     .data(allNodes)
     .enter()
     .append('circle')
     .attr('r', function(d){return nodescale(d.Probability)})
     .attr('fill', function(d){return myColor(d.Class) })
+    .classed('node', true)
+    .classed("fixed", d => d.fx !== undefined)
     .call(d3.drag()
             .on("start", onDragStarted)
             .on("drag", onDrag)
-            .on("end", onDragEnded));
+            .on("end", onDragEnded))
+    .on('click', onClick);
+  
   zoom_handler = d3.zoom().on('zoom', onZoomAction);
   zoom_handler(svg);
+
+  d3.selectAll('circle').append("text")
+        .attr("text-anchor", "middle")
+        .text(function(d) { return d.Symbol; })
+        .attr('alignment-baseline', 'middle')
+        .style('color', 'black');
 
   simulation.nodes(allNodes).on('tick', onTick);
 
@@ -113,23 +122,99 @@ $( document ).ready(function() {
   }
 
   function onZoomAction(){
-      g.attr("transform", d3.event.transform)
+    d3.selectAll('g.nodes').attr("transform", d3.event.transform)
+    d3.selectAll('g.links').attr("transform", d3.event.transform)
+    //g.attr("transform", d3.event.transform)
   }
 
   function onDragStarted(d) {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
+    d3.select(this).classed("fixed", true);
   }
 
   function onDrag(d) {
     d.fx = d3.event.x;
     d.fy = d3.event.y;
+    simulation.alpha(1).restart();
   }
 
   function onDragEnded(d) {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
+  }
+
+  function onClick(d) {
+    delete d.fx;
+    delete d.fy;
+    d3.select(selectedNode).style('stroke', 'transparent');
+    setSidebarInformation(d);
+    d3.select(this).classed("fixed", false);
+    d3.select(this).style('stroke', 'red');
+    selectedNode = this;
+    simulation.alpha(1).restart();
+  }
+
+  function setSidebarInformation(d) {
+    $('#static_id').text(d.id);
+    $('#static_class').text(d.Class);
+    $('#static_known').text(d['Known/Novel'])
+    $('#static_name').text(d.Name);
+    $('#static_prob').text(d.Probability);
+    $('#static_rank').text(d.Rank);
+    $('#static_symbol').text(d.Symbol);
+    $('#static_site').attr('href', 'https://www.ncbi.nlm.nih.gov/gene/' + d.id, '_blank');
+    $('#static_site').text('Click here');
+  }
+
+  function modifyNodeCount(startThresh, endThresh) {
+    d3.selectAll('g.nodes').remove();
+    d3.selectAll('g.links').remove();
+    //allNodes = dataset.nodes.sort((a, b) => (a.Probability - b.Probability))
+    newNodes = dataset.nodes.filter(
+      function(n) {
+        if (n.Probability >= startThresh && n.Probability <= endThresh) {
+          return true;
+        }
+        return false;
+      }
+    )
+    newLinks = dataset.links.filter(
+      function(l) {
+          for(let i = 0; i < newNodes.length; i++){
+              if (l.source.id == newNodes[i].id || l.target.id == newNodes[i].id) {
+                  return true;
+              }
+          }
+          return false;
+      }
+  )
+  console.log(newNodes);
+  console.log(newLinks);
+    linkElements = svg.append('g')
+        .attr("class", "links")
+        .selectAll("line")
+        .data(newLinks)
+        .enter().append("line")
+        .style("stroke", "#ADA9A8")
+        .style("stroke-width", function(d) { return (d.weight); });
+    nodeElements = svg.append('g')
+      .attr('class', 'nodes')
+      .selectAll('circle')
+      .data(newNodes)
+      .enter()
+      .append('circle')
+      .attr('r', function(d){return nodescale(d.Probability)})
+      .attr('fill', function(d){return myColor(d.Class) })
+      .classed('node', true)
+      .classed("fixed", d => d.fx !== undefined)
+      .call(d3.drag()
+              .on("start", onDragStarted)
+              .on("drag", onDrag)
+              .on("end", onDragEnded))
+      .on('click', onClick);
+    zoom_handler = d3.zoom().on('zoom', onZoomAction);
+    zoom_handler(svg);
+    nodeElements.append("text")
+        .attr("text-anchor", "middle")
+        .text(function(d) { return d.Symbol; })
+        .attr('alignment-baseline', 'middle');
+    simulation.alpha(1).restart();
   }
 });
