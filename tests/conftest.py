0773e7c8f6133da25e92794028d74d184020b5b6
Newsted, Jacob <newsted1@msu.edu>
2022-03-14 16:52:06 -0400

    Updated to at least the setup correct, getting an access denied error on chrome

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,18 @@
+import multiprocessing
+
 import pytest
 
 from app import app as flask_app
 
-@pytest.fixture()
+@pytest.fixture
+def chrome_options(request, chrome_options):
+    #chrome_options.headless = True
+    chrome_options.add_argument('--incognito')
+    return chrome_options
+
+@pytest.fixture(scope='session')
 def app():
+    multiprocessing.set_start_method("fork")
     flask_app.config.update({
         'TESTING': True
     })
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,6 +1,16 @@
+import pytest
 
+from selenium.webdriver.common.by import By
 
-def test_index_page(client):
-    response = client.get('http://127.0.0.1:5000/')
-    print(response)
+# To run: py.test -s --driver Chrome --driver-path ./driver/chromedriver
+# Current issue: getting an access denied on chrome
+
+# from https://pytest-flask.readthedocs.io/en/latest/features.html#start-live-server-start-live-server-automatically-default
+@pytest.mark.usefixtures('live_server')
+def test_homepage(selenium):
+    selenium.get('http://127.0.0.1:5000/')
+    print('\n---------------------------------------------------------------')
+    print(selenium.title)
+    print('---------------------------------------------------------------')
+    geneBtn = selenium.find_element(By.ID, 'geneBtn')
     return True
