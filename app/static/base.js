363bb2d4f8b269d31a11a4354f0f1f9ddec021fd
Newsted, Jacob <newsted1@msu.edu>
2022-04-12 12:18:17 -0400

    Squashed commit of the following:
    commit 830da584402df46956a16ff5bf5f6287043a10b6
    Author: Newsted, Jacob <newsted1@msu.edu>
    Date:   Tue Apr 12 11:48:12 2022 -0400
    
        Fix for #191
    
    commit 91b5aac3e7934d7bf599c58aae1ac5b747925885
    Merge: bad2adf 0b53f44
    Author: Newsted, Jacob <newsted1@msu.edu>
    Date:   Tue Apr 12 10:49:33 2022 -0400
    
        Merge branch 'dev' into 155-Popovers
    
    commit bad2adf86a0ca73f026e086d3b14448ea33d03aa
    Author: Newsted, Jacob <newsted1@msu.edu>
    Date:   Fri Mar 11 11:10:49 2022 -0500
    
        Now with proper url_for usage
    
    commit 487a63e2b967b83a30f92fd3f73f1e4948927412
    Author: Newsted, Jacob <newsted1@msu.edu>
    Date:   Fri Mar 11 10:36:50 2022 -0500
    
        Changes to popovers for the help text. Not done because we need to make it use "urlfor"
    
    commit 1389d7c3aee1f13e8a15ba72a62b08a8bc5639ba
    Author: Newsted, Jacob <newsted1@msu.edu>
    Date:   Fri Mar 11 10:20:37 2022 -0500
    
        First pass, with the example on Enter/Upload Gene List

--- app/static/base.js
+++ app/static/base.js
@@ -1,8 +1,26 @@
 $(document).ready(function(data){
+    /*
     var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
     var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
         return new bootstrap.Popover(popoverTriggerEl, {
             html: true
         })
     });
+    */
+    $("[data-bs-toggle=popover]")
+    .popover({ html: true})
+        .on("focus", function () {
+            $(this).popover("show");
+        }).on("focusout", function () {
+            var _this = this;
+            if (!$(".popover:hover").length) {
+                $(this).popover("hide");
+            }
+            else {
+                $('.popover').mouseleave(function() {
+                    $(_this).popover("hide");
+                    $(this).off('mouseleave');
+                });
+            }
+        });
 });
