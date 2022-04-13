4e9e8debb13e1d8f0601c26fcd1f2a52841b69c9
Patrick Bills <billspat@msu.edu>
2022-04-13 11:19:56 -0400

    use complex job name, simple url for example

--- copy_job_for_example.sh
+++ copy_job_for_example.sh
@@ -2,29 +2,34 @@
 
 # this will take an existing job and put it as the example 
 # this assumes the env var $JOB_PATH is set ( use source .env to set it )
-# check for parameter
+# TODO check for parameter
+
 export EXAMPLEJOB=$1
-export EXAMPLENAME="example ( Primary Ciliary Dyskinesia )"
+export EXAMPLENAME="example"  #  ( Primary Ciliary Dyskinesia )"
 cp -R "$JOB_PATH/$EXAMPLEJOB" "$JOB_PATH/$EXAMPLENAME"
 cd  "$JOB_PATH/$EXAMPLENAME"
 # rename all the files
 
-for f in $JOB_PATH/${EXAMPLEJOB}*.*; do 
+for f in ${EXAMPLEJOB}*.*; do 
     newname=${f/"$EXAMPLEJOB"/"$EXAMPLENAME"}
     mv "$f" "$newname"
 done
 
+# for this special example job, changing the name that's displayed from the name (folder) of the job
+# normally they are the same
+# this is to have a complex name displayed for the example page, but keep the URL simple to just /example
+export DISPLAYNAME="example ( Primary Ciliary Dyskinesia )"
 
 # some files have job names and job file paths ... replace those with sed
-sed_pattern="s/$EXAMPLEJOB/$EXAMPLENAME/g"
+sed_pattern="s/$EXAMPLEJOB/$DISPLAYNAME/g"
 sed -i "$sed_pattern" "$EXAMPLENAME.json"
 sed -i "$sed_pattern" job_info.json
 sed -i "$sed_pattern" results.html
     
 
 # make nicer jobs names in output files
-sed -i 's/"JOBNAME": "example"/"JOBNAME": "$EXAMPLENAME"/g' "$EXAMPLENAME.json"
-sed -i 's/"jobname": "example"/"jobname": "$EXAMPLENAME"/g' job_info.json 
+sed -i 's/"JOBNAME": "example"/"JOBNAME": "$DISPLAYNAME"/g' "$EXAMPLENAME.json"
+sed -i 's/"jobname": "example"/"jobname": "$DISPLAYNAME"/g' job_info.json 
 
 linkname=${EXAMPLENAME//" "/"%20"}
 
