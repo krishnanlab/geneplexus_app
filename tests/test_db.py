021597654d6957f2cda7a33688b3e9c67a873ce0
Newsted, Jacob <newsted1@msu.edu>
2022-08-26 14:32:23 -0400

    End of day work on this, still doesn't work though

--- tests/test_db.py
+++ tests/test_db.py
@@ -60,3 +60,35 @@ def test_login(client, session):
     rv = login(client, 'test', 'test')
 
     assert b'Username and password combination did not match' not in rv.data
+
+def test_public_accessibility(client, session):
+    private_user = User('secret', 'secret', '', '')
+    session.add(private_user)
+    session.commit()
+
+    private_job = Job('secretjob', private_user.id)
+    session.add(private_job)
+    session.commit()
+
+    private_result = Result(
+        network='BioGrid',
+        feature='Embedding',
+        negative='GO',
+        p1=0.5,
+        p2=0.5,
+        p3=0.5,
+        public=True,
+        description='This is a description',
+        userid=private_user.id,
+        jobname=private_job.jobid
+    )
+    session.add(private_result)
+    session.commit()
+
+    rv = client.get('/jobs/' + private_job.jobid, follow_redirects=True)
+    print(rv.data)
+    assert False
+    assert b'either does not exist or is private' not in rv.data
+
+    #rv = client.get('/jobs/' + 'thisiswrong', follow_redirects=True)
+    #assert b'either does not exist or is private' in rv.data
